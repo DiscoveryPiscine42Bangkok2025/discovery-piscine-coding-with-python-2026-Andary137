ce4c5ac96caaf37c495a34d4cbe972b4013767ca
Andary137 <67050207@kmitl.ac.th>
2026-02-11 00:37:18 +0700

    11feb

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,12 @@
+print("Enter a number less than 25")
+
+user_input = input()
+
+number = int(user_input)
+
+if number > 25:
+    print("Error")
+else:
+    while number <= 25:
+        print(f"Inside the loop, my variable is {number}")
+        number += 1
---
+++ cell03/ex01/multiplication_table.py
@@ -0,0 +1,7 @@
+print("Enter a number")
+
+number = int(input())
+
+for i in range(11):
+    result = i * number
+    print(f"{i} x {number} = {result}")
