787ad1868e71174c552231c239716af592844651
Andary137 <67050207@kmitl.ac.th>
2026-02-11 01:11:00 +0700

    11Feb

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,4 +1,5 @@
 first_name = "Thanakit"
+
 last_name = "Seangpan"
 
-print(first_name,last_name)
+print(first_name,last_name)      
--- cell01/ex02/age.py
+++ cell01/ex02/age.py
@@ -3,4 +3,4 @@ age = 42
 
 my_age = (my + age)
 
-print(my_age)
+print(my_age) 
